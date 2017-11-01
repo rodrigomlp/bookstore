17e662b96fcc8e39cbf98a8e26c5bc718fd870eb
Rodrigo Proenca <rodrigomlp@gmail.com>
2017-11-01 03:12:29 -0700

    Redefining routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,13 @@
 Rails.application.routes.draw do
-  get 'authors/new'
 
-  get 'authors/create'
+  #Solve this route problem
+  #root :to => "books/index"
 
-  get 'authors/update'
+  resources :books
+  resources :authors
+  resources :categories
+  resources :publishers
 
-  get 'authors/edit'
-
-  get 'authors/destroy'
-
-  get 'authors/index'
-
-  get 'authors/show'
-
-  get 'publishers/new'
-
-  get 'publishers/create'
-
-  get 'publishers/update'
-
-  get 'publishers/edit'
-
-  get 'publishers/destroy'
-
-  get 'publishers/index'
-
-  get 'publishers/show'
-
-  get 'categories/new'
-
-  get 'categories/create'
-
-  get 'categories/update'
-
-  get 'categories/edit'
-
-  get 'categories/destroy'
-
-  get 'categories/index'
-
-  get 'categories/show'
-
-  get 'books/new'
-
-  get 'books/create'
-
-  get 'books/update'
-
-  get 'books/edit'
-
-  get 'books/destroy'
-
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'about/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
